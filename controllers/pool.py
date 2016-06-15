e60920b71ad5af5d6b683f9919ed715c22837303
zvolsky <zvolsky@seznam.cz>
2016-06-15 10:32:11 +0200

    nejake pocty v revizi, neodladeno

--- controllers/pool.py
+++ controllers/pool.py
@@ -2,15 +2,25 @@
 
 @auth.requires_login()
 def review():
+    """
+    We accept for now 'l!' (dun)
+    We accept for now HACTIONS_TMP_LOST='r?' too, because we report them separately
+    HACTIONS_TMP_LOST = 'r?'  # ignore in pool revision   # we accept l! for now
+    """
+    from global_settings import HACTIONS_TMP_LOST
+
     if not auth.library_id:
         redirect(URL('default', 'index'))
     review_date = db(db.library.id == auth.library_id).select(db.library.review_date).first().review_date
-    '''
-    db.place.id.readable = False
-    grid = SQLFORM.grid(db.place,
-            searchable=False,
-            showbuttontext=False,
-            csv=False
-            )
-    '''
-    return dict(cnt_t=db(db.owned_book).count(), cnt_i=db(db.impression).count(), review_date=review_date)
+    if USE_TZ_UTC:
+        review_time = datetime.datetime(review_date.year, review_date.month, review_date.day)
+    else:  # use library tz (must be from client settings!, default for settings can be from clients javascript)
+        #review_time =
+        assert False, 'Not implemented yet.'
+    cnt_tmp_lost = db((db.impression) & (db.impr_hist.haction == HACTIONS_TMP_LOST)).count(
+                           left=db.impr_hist.on(db.impr_hist.impression_id == db.impression.id),
+                           orderby=~db.impr_hist.htime)
+    cnt_missing = db((db.impression) & (db.impr_hist.htime >= review_time)).count(
+                    left=db.impr_hist.on(db.impr_hist.impression_id == db.impression.id))
+    return dict(cnt_t=db(db.owned_book).count(), cnt_i=db(db.impression).count(),
+                cnt_tmp_lost=cnt_tmp_lost, cnt_missing=cnt_missing, review_date=review_date)
--- models/db_model.py
+++ models/db_model.py
@@ -4,35 +4,9 @@ from itertools import groupby
 
 from gluon import current
 
+from global_settings import (DEFAULT_CURRENCY, SUPPORTED_CURRENCIES, TESTING_LIB_ID,
+                             HACTIONS, HACTIONS_IN, HACTIONS_OUT, HACTIONS_MVS, HACTIONS_MVS_HINT)
 from c2_db import PublLengths
-
-
-DEFAULT_CURRENCY = 'CZK'
-SUPPORTED_CURRENCIES = ('CZK', 'EUR', 'PLZ', 'USD')
-
-TESTING_LIB_ID = 1
-
-# impressions history
-HACTIONS = (('+o', T("zaevidován zpětně")), ('+g', T("získán jako dar")), ('+n', T("zaevidován - nový nákup")),
-            ('--', T("vyřazen (bez dalších podrobností)")),
-                ('-d', T("vyřazen (likvidován)")), ('-b', T("předán ke svázání (vyřazen)")),
-                ('-g', T("vyřazen (darován)")), ('-n', T("odprodán")), ('-?', T("nezvěstný vyřazen")),
-            ('+f', T("cizí dočasně zařazen (zapůjčený výtisk)")), ('-f', T("zapůjčený cizí vyřazen (vrácen zpět - odevzdán)")),
-            ('o+', T("náš zapůjčený zařazen (byl vrácen zpět)")), ('o-', T("náš dočasně vyřazen (zapůjčen - předán)")),
-            ('l+', T("vrácen")), ('l-', T("vypůjčen")),
-            ('l!', T("upomínka")), ('ll', T("prodloužen vzdáleně")), ('lL', T("prodloužen fyzicky")),
-            ('r*', T("revidován")), ('r?', T("označen jako nezvěstný")),
-            )
-HACTIONS_IN = tuple(filter(lambda ha: ha[0][0] == '+', HACTIONS))
-HACTIONS_OUT = tuple(filter(lambda ha: ha[0][0] == '-', HACTIONS))
-HACTIONS_MVS = (('+f', T("získali jsme cizí knihy odjinud")),
-                ('-f', T("vrátili jsme cizí knihy")),
-                ('o-', T("zapůjčili jsme naše knihy jinam")),
-                ('o+', T("vrátily se nám naše knihy")))
-HACTIONS_MVS_HINT = (('+f', T("cizí knihy dočasně získávám (současně označte Příjem)")),
-                ('-f', T("cizí knihy vracím zpět - odevzdávám")),
-                ('o-', T("naše knihy zapůjčuji - předávám")),
-                ('o+', T("naše knihy se vrátily - zařazuji je zpět (je doporučeno označit Příjem)")))
 
 
 # export for modules
