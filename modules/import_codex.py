d66938c1e58466ad25272d2e80b13b04a9416eae
zvolsky <zvolsky@seznam.cz>
2016-08-31 10:28:32 +0200

    merge

--- modules/import_codex.py
+++ modules/import_codex.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
 
+"""
+    Read from dbf's with codepage unsupported by modules dbf and codecs (like 895 cz Kamenicky)
+"""
+
 import os
 
 import dbf
 from dbf_read_iffy import fix_init, fix_895
 fix_init(dbf)
+
 """
-<<<<<<< Updated upstream
-    Read from dbf's with codepage unsupported by modules dbf and codecs (like 895 cz Kamenicky)
-=======
-Read from dbf's with codepage unsupported by modules dbf and codecs (like 895 cz Kamenicky)
->>>>>>> Stashed changes
-
-import dbf
-from dbf_read_iffy import fix_init, fix_895
-
-<<<<<<< Updated upstream
+    import dbf
+    from dbf_read_iffy import fix_init, fix_895
     fix_init(dbf)
     t = dbf.Table('autori.dbf')
     t.open('read-only')
     for record in t:
         print fix_895(record.autor)
     t.close()
-=======
-fix_init(dbf)
-t = dbf.Table('autori.dbf')
-t.open('read-only')
-for record in t:
-    print fix_895(record.autor)
-t.close()
->>>>>>> Stashed changes
 """
 
 from dal_import import load_redirects
