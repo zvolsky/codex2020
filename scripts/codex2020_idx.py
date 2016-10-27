044c7d3dfbc2594841262da1a155efdee336c127
zvolsky <zvolsky@seznam.cz>
2016-10-27 13:02:21 +0200

    spoustec systemd codex2020_idx service

---
+++ scripts/codex2020_idx.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from time import sleep
+
+
+def main():
+    for i in xrange(20):
+        print i
+        sleep(3)
+
+
+if __name__ == "__main__":
+    main()
+    #raise BaseException
