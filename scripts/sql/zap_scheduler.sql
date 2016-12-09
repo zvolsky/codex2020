a2da0341b6cc01a79fb454ddbbdae74ea6651c9f
zvolsky <zvolsky@seznam.cz>
2016-12-09 15:19:58 +0100

    + sql skript

---
+++ scripts/sql/zap_scheduler.sql
@@ -0,0 +1,4 @@
+﻿truncate scheduler_run restart identity cascade;
+truncate scheduler_task restart identity cascade;
+truncate scheduler_task_deps restart identity cascade;
+truncate scheduler_worker restart identity cascade;
