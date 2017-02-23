8ccde88f0c4ef1943bc41e30488e09be81ce6f02
zvolsky <zvolsky@seznam.cz>
2017-02-23 17:59:26 +0100

    pokus odjinud

--- controllers/video.py
+++ controllers/video.py
@@ -13,5 +13,11 @@ def play3():
     return dict()
 def play3b():
     import os
-    with open(os.path.join(os.getcwd(), 'applications', request.application, 'static', 'video/6')) as v:
+    with open(os.path.join(os.getcwd(), 'applications', request.application, 'static', 'video/6.3gp')) as v:
         return v.read()
+
+def play4():
+    return dict()
+def play4b():
+    import os
+    return response.stream(os.path.join(os.getcwd(), 'applications', request.application, 'static', 'video/6.3gp'))
