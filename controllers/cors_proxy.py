e6cebb6e2c0e4c00004d39bd35f6d71bf81459c7
Mirek Zvolský <zvolsky@seznam.cz>
2017-07-25 13:46:51 +0200

    cors proxy

--- controllers/cors_proxy.py
+++ controllers/cors_proxy.py
@@ -5,4 +5,9 @@ import urllib2
 def api():
     if request.args[0] == 'zonky':
         url = 'https://api.zonky.cz/loans/marketplace?rating__eq=%s' % request.args[1]
-    return urllib2.urlopen(url).read()
+
+    if url:
+        response.headers = {'Access-Control-Allow-Origin': '*'}
+        return urllib2.urlopen(url).read()
+    else:
+        raise HTTP(403)
