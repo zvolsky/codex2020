62e8917d23c766b142d313546f572309a3f9969c
Mirek Zvolský <zvolsky@seznam.cz>
2017-08-15 15:25:17 +0200

    lepe status scheduleru

--- controllers/scheduler.py
+++ controllers/scheduler.py
@@ -3,6 +3,6 @@
 def status():
     sch = db(db.scheduler_run).select(orderby=~db.scheduler_run.id, limitby=(0,1)).first()
     if sch:
-        return '%s %s\n%s' % (sch.status, sch.start_time.strftime('%Y.%m.%d %H:%M'), sch.traceback)
+        return '%s %s<br /><br />%s' % (sch.status, sch.start_time.strftime('%Y.%m.%d %H:%M'), sch.traceback.replace('\n', '<br />'))
     else:
         return "scheduler_run: nothing found"
