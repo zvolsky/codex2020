4cb8e86c2c489b3195267b77bc10493049370261
Mirek Zvolský <zvolsky@seznam.cz>
2017-08-17 12:22:10 +0200

    ladeni scheduleru

---
+++ scripts/import_codex_direct.py
@@ -0,0 +1,20 @@
+def do_import(imp_func, library_id, src_folder=None, full=False):
+    # delayed imports
+    def init_imp_codex():
+        from import_codex import imp_codex
+        return imp_codex
+
+    auth.library_id = library_id
+    imp_func = {'imp_codex': init_imp_codex}[imp_func]()
+    if full:
+        db(db.owned_book.library_id == library_id).update(found_at_last=False)
+        db(db.impression.library_id == library_id).update(found_at_last=False)
+        db.commit()
+    print(111)
+    imp_func(db, library_id, src_folder)
+    print(222)
+    db.commit()   # to be sure; but imp_func itself should commit (in chunks or so)
+
+do_import("imp_codex", 2, src_folder="/home/www-data/web2py/applications/codex2020/imports/2/src", full=true)
+
+#{"library_id": 2, "imp_func": "imp_codex", "full": true, "src_folder": "/home/www-data/web2py/applications/codex2020/imports/2/src"}
