3c6c64dc77faeb0461bc4a202363a31ab39d5fe2
Mirek Zvolský <zvolsky@seznam.cz>
2017-08-30 14:14:50 +0200

    nastaveni: slug

--- controllers/library.py
+++ controllers/library.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from mzutils import slugify
 from plugin_mz import formstyle_bootstrap3_compact_factory
 from dal_common import hide_all_fields
 # from dal_common import ...  # in choose_library()
@@ -82,7 +83,8 @@ def library():
     """edit library info"""
     if not auth.library_id or not auth.user_id:
         redirect(URL('choose_library'))
-    important = db(db.library.id == auth.library_id).select(db.library.imp_system).first()
+    important = db(db.library.id == auth.library_id).select(
+            db.library.imp_system, db.library.slug, db.library.library).first()
     if not important:
         redirect(URL('choose_library'))
     ownership = db((db.auth_lib.auth_user_id == auth.user_id) & (db.auth_lib.library_id == auth.library_id) & (db.auth_lib.rw == True)).select().first()
@@ -101,71 +103,12 @@ def library():
     elif section == 'software':
         db.library.old_system.writable = db.library.old_system.readable = True
         db.library.imp_system.writable = db.library.imp_system.readable = True
-
-    '''
-    Field('slug', 'string', length=32,
-          requires=[IS_NOT_EMPTY(), IS_NOT_IN_DB(db, 'library.slug')],
-          label=T("URL jméno"),
-          comment=T("jméno do URL adresy [malá písmena, číslice, pomlčka/podtržítko] (příklad: petr_stary_kladno)")),
-    Field('read_pwd', 'password',
-          label=T("Heslo pro čtení"), comment=T("ponechej prázdné pro veřejně přístupný katalog")),
-    Field('created', 'datetime', default=datetime.datetime.utcnow(),
-          notnull=True, writable=False,
-          label=T("Vytvořeno"), comment=T("čas vytvoření evidence")),
-    Field('completed', 'date',
-          label=T("Dokončeno"), comment=T("datum dokončení zápisu fondu knihovny")),
-    Field('review_date', 'date', default=datetime.date.today(),
-          notnull=True, requires=[IS_NOT_EMPTY(), IS_DATE(format=T('%d.%m.%Y'))],
-          label=T("Zahájení revize"), comment=T("den zahájení revize (vypíší se výtisky, nenalezené od tohoto data)")),
-    Field('st_imp_id', 'boolean', notnull=True, default=False,  # libstyle['id'][0] = I
-          label=T("Přír.číslo ?"), comment=T("označte, pokud knihovna používá přírůstková čísla výtisků")),
-    Field('st_imp_idx', 'integer', notnull=True, default=1,
-          # libstyle['id'][1] = 0|1|2|.. which number-part of ID should be incremented
-          label=T("Typ inkrementování"), comment=T(
-            "0 nezvětšovat přír.číslo; 1 zvětšovat resp. zvětšovat první nalezené číslo; 2 zvětšovat druhé nalezené podčíslo (např. při stylu: rok/číslo)")),
-    Field('st_imp_ord', 'boolean', notnull=True, default=False,  # libstyle['id'][2] = O
-          label=T("Čís.výtisku ?"),
-          comment=T("označte, pokud se má zobrazovat číslo výtisku jako rozlišení výtisků každé publikace")),
-    Field('st_imp_rik', 'integer',  # libstyle['lrik'] = 2/3/4/5/6
-          notnull=True, default=3, requires=IS_INT_IN_RANGE(2, 7),
-          label=T("Rychlá identifikace"), comment=T(
-            "[DŮLEŽITÉ: později NEMĚNIT!] kolikamístné číslo používat pro rychlé hledání knihy z klávesnice? zvol podle velikosti knihovny: 2 - do počtu 50 výtisků, 3 - do 500, 4 - do 5000, 5 - do 50000, 6 - nad 50000")),
-    Field('st_imp_bc', 'boolean', notnull=True, default=False,  # libstyle['bc'][0] = B
-          label=T("Čarové kódy ?"), comment=T("označte, pokud knihovna používá vlastní čarové kódy")),
-    Field('st_imp_bc2', 'boolean', notnull=True, default=True,  # libstyle['bc'][1] = +
-          label=T("Inkremetovat čar.kódy ?"), comment=T(
-            "Ano: čarový kód více výtisků bude předvyplněn zvětšujícím se číslem; Ne: čar.kód 2+ výtisku doplníte ručně")),
-    Field('st_imp_pl', 'boolean', notnull=True, default=False,  # libstyle['gr'][0] = P
-          label=T("Umístění výtisku ?"),
-          comment=T("označte, pokud chcete zapisovat, kde je výtisk umístěn (oddělení, místnost, regál, apod.)")),
-    Field('st_imp_sg', 'boolean', notnull=True, default=False,  # libstyle['sg'][0] = G
-          label=T("Signatura výtisku ?"),
-          comment=T("označte, pokud používáte signatury a každý výtisk má mít unikátní")),
-    Field('st_imp_sgsep', 'string', length=3, notnull=True, default='',  # libstyle['sgsep']
-          label=T("Oddělovač v signatuře"),
-          comment=T("unikátní signatura výtisku (je-li použita): znak(y) pro oddělení dodatku")),
-    Field('st_imp_sgmod1', 'string', length=1, notnull=True, default='',  # libstyle['sg'][1]
-          label=T("Signatura, 1.výtisk"),
-          comment=T("unikátní signatura výtisku (je-li použita): přídavný znak 1.výtisku (např. prázdný, a, A, 1)")),
-    Field('st_imp_sgmod2', 'string', length=1, notnull=True, default='b',  # libstyle['sg'][2]
-          label=T("Signatura, 2.výtisk"),
-          comment=T("unikátní signatura výtisku (je-li použita): přídavný znak 2.výtisku (např. a, b, B, 2)")),
-    Field('st_imp_st', 'boolean', notnull=True, default=False,  # libstyle['gr'][1] = s
-          label=T("Stat.dělení výtisků ?"), comment=T(
-            "označte, pokud chcete pro účel statistiky rozdělovat výtisky (tip: i pro oddělení dosp/děts, pokud výtisky titulu mohou být přiděleny do různých oddělení)")),
-    Field('st_tit_st', 'boolean', notnull=True, default=False,  # libstyle['gr'][2] = S
-          label=T("Stat.dělení titulů ?"), comment=T("označte, pokud chcete pro účel statistiky rozdělovat tituly")),
-    Field('imp_total', 'integer', readable=False, default=0,
-          label=T("Počet v importu"), comment=T("počet publikací, které budou/byly celkově importovány")),
-    Field('imp_proc', 'decimal(5,2)', readable=False, writable=False, default=100.0),  # import position in %
-    Field('imp_done', 'integer', readable=False, default=0,
-          label=T("Počet již importovaných"),
-          comment=T("počet již importovaných publikací celkem (nových i existujících)")),  # imp_done cnt
-    Field('imp_new', 'integer', readable=False, default=0,
-          label=T("Počet nových"), comment=T("počet nových již importovaných publikací")),  # imp_new cnt
-    Field('last_import', 'datetime', writable=False,
-          label=T("Naposledy importováno"), comment=T("čas posledního importu z jiného systému")),
-    '''
+    elif section == 'publish':
+        db.library.is_public.writable = db.library.is_public.readable = True
+        db.library.slug.writable = db.library.slug.readable = True
+        if not important.slug and important.library:
+            db.library[auth.library_id] = slugify(important.library)
+            db.commit()
 
     form = SQLFORM(db.library, auth.library_id,
                    formstyle=formstyle_bootstrap3_compact_factory(),
